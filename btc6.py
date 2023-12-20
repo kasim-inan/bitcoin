1934fa1f3f2b8c405289154f374cf49595619cb1
kasim <114011990+kasim-inan@users.noreply.github.com>
2023-12-19 23:56:18 -0800

    Update btc6.py

--- btc6.py
+++ btc6.py
@@ -1,95 +1,29 @@
-'''
-import streamlit as st
-import requests
-import matplotlib.pyplot as plt
-from datetime import datetime
-
-# Function to fetch current Bitcoin price
-def get_bitcoin_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-    response = requests.get(url)
-    data = response.json()
-    return data['bitcoin']['usd']
-
-# Function to fetch historical Bitcoin prices
-def get_historical_prices(days=30):
-    url = f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days={days}"
-    response = requests.get(url)
-    data = response.json()
-    prices = data['prices']
-    return [(datetime.fromtimestamp(int(item[0])/1000), item[1]) for item in prices]
-
-# Function to plot the price chart
-def plot_price_chart(prices):
-    dates = [item[0] for item in prices]
-    values = [item[1] for item in prices]
-    plt.figure(figsize=(10, 4))
-    plt.plot(dates, values, label="Bitcoin Price", color='orange')
-    plt.xlabel("Date")
-    plt.ylabel("Price in USD")
-    plt.title("Bitcoin Price Chart")
-    plt.legend()
-    return plt
-
-# Main function for the Streamlit app
-def main():
-    st.title('Bitcoin Price Tracker')
-
-    # Display current Bitcoin price
-    try:
-        bitcoin_price = get_bitcoin_price()
-        # Change the color and size of the current price display
-        st.markdown(f"<h2 style='color: orange;'>The current price of Bitcoin is: ${bitcoin_price}</h2>", unsafe_allow_html=True)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-
-    # Display historical Bitcoin price chart
-    try:
-        prices = get_historical_prices()
-        fig = plot_price_chart(prices)
-        st.pyplot(fig)
-    except Exception as e:
-        st.error(f"An error occurred when fetching historical data: {e}")
-
-# Ensuring the main function runs when the script is executed
-if __name__ == "__main__":
-    main()
-
-    
-'''
-
-
 import streamlit as st
 import requests
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
 
-# Caching the response for a certain duration can prevent hitting API rate limits and improve performance
 @st.cache(ttl=300)  # Cache for 5 minutes
 def get_bitcoin_price():
-    try:
-        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-        response = requests.get(url)
-        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    response = requests.get(url)
+    if response.status_code == 200:
         data = response.json()
         return data['bitcoin']['usd']
-    except Exception as e:
-        st.error(f"An error occurred while fetching current Bitcoin price: {e}")
+    else:
         return None
 
 @st.cache(ttl=300)  # Cache for 5 minutes
 def get_historical_prices(days=30):
-    try:
-        url = f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days={days}"
-        response = requests.get(url)
-        response.raise_for_status()
+    url = f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days={days}"
+    response = requests.get(url)
+    if response.status_code == 200:
         data = response.json()
         prices = data['prices']
         return [(datetime.fromtimestamp(int(item[0])/1000), item[1]) for item in prices]
-    except Exception as e:
-        st.error(f"An error occurred when fetching historical data: {e}")
-        return []
+    else:
+        return None
 
 def plot_price_chart(prices):
     if not prices:
@@ -115,6 +49,8 @@ def main():
     bitcoin_price = get_bitcoin_price()
     if bitcoin_price is not None:
         st.markdown(f"<h2 style='color: orange;'>The current price of Bitcoin is: ${bitcoin_price}</h2>", unsafe_allow_html=True)
+    else:
+        st.error("An error occurred while fetching the current Bitcoin price.")
 
     prices = get_historical_prices()
     if prices:
@@ -123,6 +59,8 @@ def main():
             st.pyplot(fig)
         else:
             st.error("Failed to plot the price chart.")
+    else:
+        st.error("An error occurred when fetching historical data.")
 
 if __name__ == "__main__":
     main()
